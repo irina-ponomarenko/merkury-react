72d4137e1da39ab6d8f8ab0362e7478c4f927af5
Admin <ponomarenko02041993@gmail.com>
2018-03-01 00:58:43 +0200

    new

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Home.css';
 import BoxWrapper from '../atoms/BoxWrapper';
 import Select from '../atoms/Select';
 import ReactHighcharts from 'react-highcharts';
@@ -31,6 +32,145 @@ class Home extends React.Component {
                             <ReactHighcharts config={ReportChart}></ReactHighcharts>
                         </BoxWrapper>
                     </div>
+                    <BoxWrapper className="wrapperMessage">
+                        <header className="TitleMessage">
+                            <h4>Tasks</h4>
+                            <div className="namberMessage">
+                                <div className="blueNamber">
+                                    <span>5</span>
+                                </div>
+                                <div className="redNamber">
+                                    <span>2</span>
+                                </div>
+                            </div>
+                        </header>
+                        <div className="message">
+                            <div className="wrapperBlock">
+                                <div className="iconUser">
+                                    <span>N</span>
+                                </div>
+                                <div className="titleBlock">
+                                    <p>New website for Symu.co</p>
+                                    <span> <i className="fa fa-clock-o"></i> 5 days delays</span>
+                                </div>
+                            </div>
+                            <div className="option">
+                                <i className="fa fa-ellipsis-v"></i>
+                            </div>
+                        </div>
+                        <div className="message">
+                            <div className="wrapperBlock">
+                                <div className="iconUser">
+                                    <span>F</span>
+                                </div>
+                                <div className="titleBlock">
+                                    <p>Free business PSD Template </p>
+                                    <span> <i className="fa fa-clock-o"></i> 2 days delays</span>
+                                </div>
+                            </div>
+                            <div className="option">
+                                <i className="fa fa-ellipsis-v"></i>
+                            </div>
+                        </div>
+                        <div className="message">
+                            <div className="wrapperBlock">
+                                <div className="iconUser">
+                                    <span>N</span>
+                                </div>
+                                <div className="titleBlock greySpan">
+                                    <p>New logo for JCD.pl</p>
+                                    <span> <i className="fa fa-clock-o"></i> 5 days left</span>
+                                </div>
+                            </div>
+                            <div className="option">
+                                <i className="fa fa-ellipsis-v"></i>
+                            </div>
+                        </div>
+                        <div className="message">
+                            <div className="wrapperBlock">
+                                <div className="iconUser">
+                                    <span>F</span>
+                                </div>
+                                <div className="titleBlock greySpan">
+                                    <p>Free Icons Set vol. 3</p>
+                                    <span> <i className="fa fa-clock-o"></i> 10 days left</span>
+                                </div>
+                            </div>
+                            <div className="option">
+                                <i className="fa fa-ellipsis-v"></i>
+                            </div>
+                        </div>
+                    </BoxWrapper>
+                    <BoxWrapper className="wrapperMessage">
+                        <header className="TitleMessage">
+                            <h4>Messages</h4>
+                            <div className="namberMessage">
+                                <div className="blueNamber">
+                                    <span>2</span>
+                                </div>
+                            </div>
+                        </header>
+                        <div className="message messagePadding">
+                            <div className="wrapperBlock">
+                                <div className="iconUser">
+                                    <span>N</span>
+                                </div>
+                                <div className="titleBlock newMessage">
+                                    <h4>Nina Jones <span>5 minutes ago</span></h4>
+                                    <p>Hey You! It’s me again :-) I attached new (...)</p>
+                                    <div className="shareMessage">
+                                        <i className="fa fa-share"></i>
+                                        <i className="fa fa-cog"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="message messagePadding">
+                            <div className="wrapperBlock">
+                                <div className="iconUser">
+                                    <span>F</span>
+                                </div>
+                                <div className="titleBlock newMessage">
+                                    <h4>Nina Jones <span>About 20 hours ago</span></h4>
+                                    <p>Hey! I attached some new PSD files for (...)</p>
+                                    <div className="shareMessage">
+                                        <i className="fa fa-share"></i>
+                                        <i className="fa fa-cog"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="message messagePadding">
+                            <div className="wrapperBlock">
+                                <div className="iconUser">
+                                    <span>N</span>
+                                </div>
+                                <div className="titleBlock newMessage">
+                                    <h4>James Smith <span>2 days ago</span></h4>
+                                    <p>Good morning, you are fired!</p>
+                                    <div className="shareMessage">
+                                        <i className="fa fa-share"></i>
+                                        <i className="fa fa-cog"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="message messagePadding">
+                            <div className="wrapperBlock">
+                                <div className="iconUser">
+                                    <span>F</span>
+                                </div>
+                                <div className="titleBlock newMessage">
+                                    <h4>Nina Jones <span>About 2 weeks ago</span></h4>
+                                    <p>Hello! Could You bring me coffee please?</p>
+                                    <div className="shareMessage">
+                                        <i className="fa fa-share"></i>
+                                        <i className="fa fa-cog"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </BoxWrapper>
                 </div>
             </div>
         )
